41b4d997b0f8749570b1e1ff84ca53171ba10013
Анна Зуйкова <azula6850@gmail.com>
2024-05-09 21:39:59 +0600

    modified lab8

--- Lab8/src/main/java/ReflectionDemo.java
+++ Lab8/src/main/java/ReflectionDemo.java
@@ -1,17 +1,32 @@
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ReflectionDemo {
-    public int countHuman(List<Object> list) {
+    public static int countHuman(List<Object> list) {
         int res = 0;
         for (Object o : list) {
             if (o instanceof Human) res++;
         }
         return res;
     }
-    public List<String> getPublicMethods(Object o){
-        List<String> res = new ArrayList<>();
 
+    public static List<String> getPublicMethods(Object o) {
+        List<String> res = new ArrayList<>();
+        Method[] methods = o.getClass().getMethods();
+        for (Method method : methods) {
+            res.add(method.getName());
+        }
+        return res;
+    }
+    public static List<String> getListSuperClasses(Object o) throws ClassNotFoundException,NullPointerException {
+        List<String> res = new ArrayList<>();
+            String superClassName = o.getClass().getSuperclass().getName();
+            res.add(superClassName);
+            while (!superClassName.equals("java.lang.Object")) {
+                superClassName = Class.forName(superClassName).getSuperclass().getName();
+                res.add(superClassName);
+            }
+        return res;
     }
 }
